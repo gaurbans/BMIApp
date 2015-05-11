f0a6a0e80d74f4861a96a55d32b41c1061c15db2
gaurbans <gaurbans@gmail.com>
2015-05-11 11:26:57 -0400

    first commit

--- ui.R
+++ ui.R
@@ -4,17 +4,18 @@ shinyUI(
     # Application title
     headerPanel("BMI Calculator"),
     sidebarPanel(
-      numericInput('weight', 'Weight (kg)', 70),
-      numericInput('height', 'height (m)', 2),
+      h5('Enter your weight and height to calculate your body mass index (BMI). Your BMI is your weight divided by your height squared: BMI=Weight/(Height^2)'),
+      numericInput('weight', 'Weight (kg)', 70, step=.1),
+      numericInput('height', 'height (m)', 2, step=.01),
       submitButton('Submit')
     ),
     mainPanel(
-      h3('Results of prediction'),
+      h3('Results'),
       h4('You entered a weight of'),
       verbatimTextOutput("inputValue1"),
       h4('You entered a height of'),
       verbatimTextOutput("inputValue2"),
-      h4('Which resulted in a BMI Index of '),
+      h3('Calculated BMI Index'),
       verbatimTextOutput("prediction")
     )
   )
